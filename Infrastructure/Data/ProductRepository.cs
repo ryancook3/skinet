b0ee23d2e190418d6208fc5d1a85d436edfed50b
ryancook3 <ryancook96@yahoo.com>
2022-06-11 20:50:13 -0700

    New Migration and Entities

---
+++ Infrastructure/Data/ProductRepository.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Interfaces;
+using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly StoreContext _context;
+
+        public ProductRepository(StoreContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Product> GetProductbyIdAsync(int id)
+        {
+            return await _context.Products.FindAsync(id);
+        }
+
+        public async Task<IReadOnlyList<Product>> GetProductsAsync()
+        {
+            return await _context.Products.ToListAsync();
+        }
+    }
+}
