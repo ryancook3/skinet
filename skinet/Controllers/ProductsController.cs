2d30ca84bd494b597a0bfea0446f6f4001c0636b
ryancook3 <ryancook96@yahoo.com>
2022-06-13 21:54:36 -0700

    added exceptions, and organized startup.cs

--- skinet/Controllers/ProductsController.cs
+++ skinet/Controllers/ProductsController.cs
@@ -6,12 +6,12 @@ using Core.Interfaces;
 using Core.Specifications;
 using API.Dto;
 using AutoMapper;
+using API.Errors;
 
 namespace API.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class ProductsController : ControllerBase
+
+    public class ProductsController : BaseApiController
     {
         private readonly IGenericRepository<Product> _productsRepo;
         private readonly IGenericRepository<ProductBrand> _productBrandRepo;
@@ -40,11 +40,17 @@ namespace API.Controllers
         }
 
         [HttpGet ("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status404NotFound)]
+
         public async Task<ActionResult<ProductToReturnDto>> GetProducts(int id)
         {
             var spec = new ProductsWithTypesAndBrandsSpecification(id);
 
             var product = await _productsRepo.GetEntityWithSpec(spec);
+
+            if (product == null) return NotFound(new ApiResponse(404));
+
             return _mapper.Map<Product, ProductToReturnDto>(product);
         }
 
